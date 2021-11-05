945a82932625666e74493748e39341b140dd942b
joaocarvalhop <jppcarvalho8@gmail.com>
2021-11-05 11:41:13 -0300

    DoWhile Revisão

--- src/controle/DoWhile.java
+++ src/controle/DoWhile.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class DoWhile {
 
 	public static void main(String[] args) {
-		
+
 		Scanner entrada = new Scanner(System.in);
 
 		String texto = "";
-		
-		do { System.out.println("Vc precisa falar as palavras mágicas...");
-		System.out.println("Vc quer sair???");
-		} while(!texto.equalsIgnoreCase("por favor"));
-		
+
+		do {
+			System.out.println("Vc precisa falar as palavras mágicas...");
+			System.out.println("Quer sair???");
+			texto = entrada.nextLine();
+		} while (!texto.equalsIgnoreCase("por favor"));
+
 		System.out.println("Obrigado!");
 		entrada.close();
 	}
