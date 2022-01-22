c8fb2edd9e17c100faf92994bd9f0589322d2e23
joaocarvalhop <86267920+joaocarvalhop@users.noreply.github.com>
2022-01-22 10:40:47 -0300

    last commit

--- src/arrays/desafio/Desafio1.java
+++ src/arrays/desafio/Desafio1.java
@@ -6,22 +6,21 @@ import java.util.Scanner;
 public class Desafio1 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner entrada = new Scanner(System.in);
-		
-		
+
 		System.out.print("Quantas notas deseja informar? ...");
 		int a = entrada.nextInt();
-		
+
 		double[] notas = new double[a];
-		
+
 		for (int i = 0; i < notas.length; i++) {
 			System.out.print("Informe a nota: " + (i + 1) + ": ");
 			notas[i] = entrada.nextDouble();
 		}
-		
+
 		System.out.println(Arrays.toString(notas));
-		
+
 		entrada.close();
 	}
 
